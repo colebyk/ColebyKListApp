129a84f4ca3763b14bc1600163820d03a45e73ca
colebyk <kauffman.coleby@gmail.com>
2019-03-18 11:47:49 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/colebyklistapp/ItemAdapter.java
@@ -0,0 +1,60 @@
+package com.example.colebyklistapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ItemAdapter extends BaseAdapter {
+
+    LayoutInflater mInflater;
+
+    String[] items;
+    String[] prices;
+    String[] descriptions;
+
+    public ItemAdapter(Context c, String[] i, String[] p, String[] d) {
+        items = i;
+        prices = p;
+        descriptions = d;
+        mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = mInflater.inflate(R.layout.my_listview_detail, null);
+        TextView nameTextView = (TextView) v.findViewById(R.id.nameTextView);
+        TextView priceTextView = (TextView) v.findViewById(R.id.priceTextView);
+        TextView descriptionTextView = (TextView) v.findViewById(R.id.descriptionTextView);
+
+        String name = items[position];
+        String desc = descriptions[position];
+        String cost = prices[position];
+
+        nameTextView.setText(name);
+        descriptionTextView.setText(desc);
+        priceTextView.setText(cost);
+
+        return v;
+    }
+}
